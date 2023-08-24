24954ba7ca46e15549a8ede366de7a103f75aa3c
dnfbd <dnfbd@naver.com>
2023-08-24 23:03:07 +0900

    code refactoring after components lower the bond add react Performance optimization

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,36 +1,46 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Header = styled.div`
   display:flex;
   align-items: center;
   gap:5px;
   margin-top:30px;
-`
+`;
+
 const Text = styled.span`
-  font-size : 14px;
+  font-size: 14px;
   font-weight: 600;
-`
+`;
+
 const ScrapImg = styled.img`
-  width:20px;
-  height:20px;
-  cursor:pointer;
-`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+`;
 
+const ScrapResultTitle = React.memo(({ title }) => <Text>{title}</Text>);
 
-const Nav = ({ scrapResultToggle, setScrapResultToggle, title }) => {
+const ScrapResultToggle = React.memo(({ scrapResultToggle, setScrapResultToggle }) => {
   const toggleImgSrc = scrapResultToggle ? '/images/scrap.png' : '/images/no_scrap.png';
   const toggleAltText = scrapResultToggle ? 'Scrap' : 'No Scrap';
+  
+  return (
+    <ScrapImg
+      src={toggleImgSrc}
+      alt={toggleAltText}
+      onClick={() => setScrapResultToggle(!scrapResultToggle)}
+    />
+  );
+});
+
+const Nav = ({ scrapResultToggle, setScrapResultToggle, title }) => {
   return (
     <Header>
-      <ScrapImg
-        src={toggleImgSrc}
-        alt={toggleAltText}
-        onClick={() => setScrapResultToggle(!scrapResultToggle)}
-      />
-      <Text>{title}</Text>
+      <ScrapResultToggle scrapResultToggle={scrapResultToggle} setScrapResultToggle={setScrapResultToggle} />
+      <ScrapResultTitle title={title} />
     </Header>
   );
 };
 
-
-export default Nav
+export default Nav;
